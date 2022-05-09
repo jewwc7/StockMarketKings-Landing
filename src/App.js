84f5a26cf30fb09c73ae63621ce7fc26b667ebe3
jewwc7 <67772782+jewwc7@users.noreply.github.com>
2022-05-09 15:53:05 -0500

    update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {
   Hidden,
   Container,
 } from "@mui/material";
-import DehazeIcon from "@material-ui/icons/Dehaze";
+//import DehazeIcon from "@material-ui/icons/Dehaze";
 
 import sectionOne from "./Photos/sectionOne.svg";
 import appIcon from "./Photos/appIcon.svg";
@@ -31,7 +31,7 @@ function App() {
         item
         container
         xs={12}
-        style={{ backgroundColor: "#F5FFFF", height: 500 }}
+        style={{ backgroundColor: "white", height: 500 }}
       >
         <FirstSection />
       </Grid>
@@ -47,7 +47,7 @@ function App() {
         item
         container
         xs={12}
-        style={{ backgroundColor: "#F5FFFF", paddingTop: 32 }}
+        style={{ backgroundColor: "#E9ECF2", paddingTop: 32 }}
       >
         <ThirdSection />
       </Grid>
@@ -63,10 +63,11 @@ function App() {
         item
         container
         xs={12}
-        style={{ backgroundColor: "#F5FFFF", paddingTop: 32 }}
+        style={{ backgroundColor: "#E9ECF2", paddingTop: 32 }}
       >
         <FifthSection />
       </Grid>
+      <Footer />
     </div>
   );
 }
@@ -82,6 +83,8 @@ const FirstSection = () => {
         //   alignItems: "center",
         height: 500,
         position: "relative",
+        borderBottomColor: "red",
+        borderBottomWidth: 3,
       }}
     >
       <Grid xs={12} md={6} container item alignItems={"center"}>
@@ -144,13 +147,6 @@ const SecondSection = () => {
       xs={12}
       style={{ justifyContent: "space-evenly", alignItems: "center" }}
     >
-      <Grid item xs={12} md={5}>
-        <h1>Create Funds and Test Strategies</h1>
-        <p>
-          Based on real stock market data, you’ll create funds(basket of stocks)
-          and track their performance over time.
-        </p>
-      </Grid>
       <Grid item xs={12} md={6}>
         <img
           src={sectionOne}
@@ -163,6 +159,16 @@ const SecondSection = () => {
             // filter: "brightness(80%)",
           }}
         />
+      </Grid>
+      <Grid item xs={12} md={5}>
+        <h1>
+          <span className="boxed-span">Create </span>Funds and{" "}
+          <span className="boxed-span">Test</span> Strategies
+        </h1>
+        <p>
+          Based on real stock market data, you’ll create funds(basket of stocks)
+          and track their performance over time.
+        </p>
       </Grid>
     </Grid>
   );
@@ -180,6 +186,15 @@ const ThirdSection = () => {
         //  position: "sticky",
       }}
     >
+      <Grid item xs={12} md={5}>
+        <h1>
+          Start <span className="boxed-span">Competitions</span>
+        </h1>
+        <p>
+          Place your created funds up against other users. You decide the
+          amount, competitors allowed, max stocks allowed and more!
+        </p>
+      </Grid>
       <Grid item xs={12} md={6}>
         <img
           src={sectionOne}
@@ -191,13 +206,6 @@ const ThirdSection = () => {
             // filter: "brightness(80%)",
           }}
         />
-      </Grid>
-      <Grid item xs={12} md={5}>
-        <h1>Start Competitions</h1>
-        <p>
-          Place your created funds up against other users. You decide the
-          amount, competitors allowed, max stocks allowed and more!
-        </p>
       </Grid>
     </Grid>
   );
@@ -211,13 +219,6 @@ const FourthSection = () => {
       xs={12}
       style={{ justifyContent: "space-evenly", alignItems: "center" }}
     >
-      <Grid item xs={12} md={5}>
-        <h1 style={{ color: "#F5FFFF" }}>Browse the Community</h1>
-        <p style={{ color: "#F5FFFF" }}>
-          Scroll the community to see others created funds and their
-          performance. View and join other competitions
-        </p>
-      </Grid>
       <Grid item xs={12} md={6}>
         <img
           src={sectionOne}
@@ -229,6 +230,18 @@ const FourthSection = () => {
             // filter: "brightness(80%)",
           }}
         />
+      </Grid>
+      <Grid item xs={12} md={5}>
+        <h1 style={{ color: "white" }}>
+          Browse the{" "}
+          <span className="boxed-span" style={{ backgroundColor: "#151515" }}>
+            Community
+          </span>
+        </h1>
+        <p style={{ color: "white" }}>
+          Scroll the community and see others created funds and their
+          performance. View and join other competitions
+        </p>
       </Grid>
     </Grid>
   );
@@ -242,6 +255,13 @@ const FifthSection = () => {
       xs={12}
       style={{ justifyContent: "space-evenly", alignItems: "center" }}
     >
+      <Grid item xs={12} md={5}>
+        <h1>Build the Greatest Empire</h1>
+        <p>
+          You make(and lose) money by competing and your created funds
+          performance.Will you come out on top?{" "}
+        </p>
+      </Grid>
       <Grid item xs={12} md={6}>
         <img
           src={sectionOne}
@@ -254,133 +274,20 @@ const FifthSection = () => {
           }}
         />
       </Grid>
-      <Grid item xs={12} md={5}>
-        <h1>Build the Greatest Empire</h1>
-        <p>
-          You make(and lose) money by competing and your created funds
-          performance.Will you come out on top?{" "}
-        </p>
-      </Grid>
-    </Grid>
-  );
-};
-
-const OldTopNav = ({ setOutline, outline }) => {
-  const [modalVisible, setModalVisible] = useState(false);
-  const redBackgroundColor = "rgba(65,0,0,.9)";
-  const history = useNavigate();
-  async function goToPage(page) {
-    history.push(page);
-  }
-
-  return (
-    <Grid
-      item
-      container
-      xs={12}
-      style={{
-        //    justifyContent: "space-between",
-        alignItems: "center",
-        height: 100,
-        paddingLeft: 32,
-        paddingRight: 32,
-      }}
-    >
-      <Grid
-        item
-        container
-        justifyContent="flex-start"
-        alignItems="center"
-        style={{
-          height: "80%",
-          paddingLeft: 8,
-          cursor: "pointer",
-          // backgroundColor: "blue",
-        }}
-        xs={6}
-        onClick={() => {
-          goToPage("/");
-          setOutline({ home: true });
-        }}
-      >
-        <img src={appIcon} alt="TuffHammer Logo" style={{ height: "100%" }} />
-      </Grid>
-      <Grid
-        item
-        style={{
-          paddingRight: "5%",
-          justifyContent: "flex-end",
-          display: "flex",
-        }}
-        xs={6}
-      >
-        <Hidden smDown>
-          <Button
-            variant={outline.home ? "outlined" : null}
-            onClick={() => {
-              goToPage("/");
-              setOutline({ home: true });
-            }}
-          >
-            <p className="nav-btns">Home</p>
-          </Button>
-          <Button
-            onClick={() => {
-              goToPage("/");
-              setOutline({ home: true });
-
-              setTimeout(() => {
-                window.scrollTo({
-                  top: 750,
-                  behavior: "smooth",
-                });
-              }, 500);
-            }}
-          >
-            <p className="nav-btns">Services</p>
-          </Button>
-          <Button
-            className="nav-btns"
-            variant={outline.projects ? "outlined" : null}
-            onClick={() => {
-              goToPage("/projects");
-              setOutline({ projects: true });
-            }}
-          >
-            <p className="nav-btns">Projects</p>
-          </Button>
-          <Button
-            onClick={() => {
-              goToPage("/");
-              setOutline({ home: true });
-              setTimeout(() => {
-                window.scrollTo({
-                  //this is how you scroll to bottom of page, used timeout so the new pages coordinates can be used(w/o timeout will scroll as far as prior page went down)
-                  top: document.body.scrollHeight,
-                  behavior: "smooth",
-                });
-              }, 500);
-            }}
-          >
-            <p className="nav-btns">Contact Us/Jobs</p>
-          </Button>
-        </Hidden>
-        <Hidden mdUp>
-          <IconButton>
-            <DehazeIcon
-              //     style={{color: '#003E3A'}} //not working
-              onClick={() => setModalVisible(!modalVisible)}
-            />
-          </IconButton>
-        </Hidden>
-      </Grid>
     </Grid>
   );
 };
 
 const TopNav = () => {
   return (
-    <Grid item container style={{ height: 80, alignItems: "center" }}>
+    <Grid
+      item
+      container
+      style={{
+        height: 80,
+        alignItems: "center",
+      }}
+    >
       <Grid
         item
         container
@@ -429,14 +336,20 @@ const TopNav = () => {
           </Button>
         </Hidden>
         <Hidden mdUp>
-          <IconButton>
-            <DehazeIcon
-            //     style={{color: '#003E3A'}} //not working
-            // onClick={() => setModalVisible(!modalVisible)}
-            />
-          </IconButton>
+          <IconButton></IconButton>
         </Hidden>
       </Grid>
+    </Grid>
+  );
+};
+
+const Footer = () => {
+  return (
+    <Grid item container xs={12}>
+      <Grid item container xs={12} justifyContent="center">
+        <h2>Follow Us</h2>
+      </Grid>
+      <Grid item container xs={12}></Grid>
     </Grid>
   );
 };
