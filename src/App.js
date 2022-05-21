6cc1e47ea08f0de8d7a26821598a24b5c7c68131
jewwc7 <67772782+jewwc7@users.noreply.github.com>
2022-05-20 20:02:32 -0500

    Scroll Update and fade animations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect, useRef } from "react";
 
 import "./App.css";
-//import TopNav from "./TopNav";
 import { useScrollPosition } from "@n8tb1t/use-scroll-position";
 import { SwitchTransition, CSSTransition } from "react-transition-group";
 import {
@@ -15,6 +14,8 @@ import {
   useTheme,
   createTheme,
   Icon,
+  Slide,
+  Fade,
 } from "@mui/material";
 import DehazeIcon from "@mui/icons-material/Dehaze";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
@@ -70,7 +71,7 @@ function App() {
     else return {};
   }
   function getPhotoDimensions() {
-    if (mobile) return 300;
+    if (mobile) return 360;
     if (tablet) return 600;
     if (largeTablet) return 600;
     if (laptop) return 800;
@@ -89,6 +90,65 @@ function App() {
     } else return {};
   }
 
+  const firstSectionHeight = mobile ? 800 : tablet ? 800 : 800;
+
+  /////////////////////////////////////////////////////////////////////////////////////////
+  const [inViewSecondSec, setInViewSecondSec] = useState(false);
+  const [inViewThirdSec, setInViewThirdSec] = useState(false);
+  const [inViewFourthSec, setInViewFourthSec] = useState(false);
+  const [inViewFifthSec, setInViewFifthSec] = useState(false); //controls transition in
+
+  const secondSectionRef = useRef();
+  const thirdSectionRef = useRef();
+  const fourthSectionRef = useRef();
+  const fifthSectionRef = useRef();
+
+  //2nd sec
+  useScrollPosition(({ prevPos, currPos }) => {
+    const postiveCurrPosy = Math.abs(currPos.y); //turns number postive, scroll postion is negative number
+    const { offsetTop } = secondSectionRef.current; //where the element ends with respect to viewport(bottom of element)
+    const topOfEl = offsetTop - postiveCurrPosy - 200; //this is how to get top of element (the 100 is optional but makes whatever you want happen 100px earlier )
+    // console.log(secondSectionRef.current.getBoundingClientRect()); //this gives the top position of the element with respect to current scroll postion.
+    if (postiveCurrPosy >= topOfEl) {
+      setInViewSecondSec(true); //set to to true to see transition
+    }
+  });
+  //3rd sec
+  useScrollPosition(({ prevPos, currPos }) => {
+    const postiveCurrPosy = Math.abs(currPos.y); //turns number postive, scroll postion is negative number
+    const { offsetTop } = thirdSectionRef.current; //where the element ends with respect to viewport(bottom of element)
+    const topOfEl = offsetTop - postiveCurrPosy + 900; //this is how to get top of element (the 100 is optional but makes whatever you want happen 100px earlier )
+    //  console.log({ offsetTop, postiveCurrPosy, topOfEl });
+
+    if (postiveCurrPosy >= topOfEl) {
+      setInViewThirdSec(true); //set to to true to see transition
+    }
+  });
+  //4th sec
+  useScrollPosition(({ prevPos, currPos }) => {
+    const postiveCurrPosy = Math.abs(currPos.y); //turns number postive, scroll postion is negative number
+    const { offsetTop } = fourthSectionRef.current; //where the element ends with respect to viewport(bottom of element)
+    const topOfEl = offsetTop - postiveCurrPosy + 1200; //this is how to get top of element (the 100 is optional but makes whatever you want happen 100px earlier )
+    if (postiveCurrPosy >= topOfEl) {
+      setInViewFourthSec(true); //set to to true to see transition
+    }
+  });
+  //5th sec
+  useScrollPosition(({ prevPos, currPos }) => {
+    const postiveCurrPosy = Math.abs(currPos.y); //turns number postive, scroll postion is negative number
+    const { offsetTop } = fifthSectionRef.current; //where the element ends with respect to viewport(bottom of element)
+    const topOfEl = Math.abs(offsetTop - postiveCurrPosy) + 1800; //this is how to get top of element (the 100 is optional but makes whatever you want happen 100px earlier )
+    //console.log({ offsetTop, postiveCurrPosy, topOfEl });
+    if (postiveCurrPosy >= topOfEl) {
+      setInViewFifthSec(true); //set to to true to see transition
+    }
+  });
+
+  const fadeTimeout = {
+    appear: 0,
+    enter: 300,
+  };
+
   const pageProps = {
     photoHeight,
     mobile,
@@ -97,10 +157,14 @@ function App() {
     laptop,
     largeTablet,
     textAlignment,
+    inViewSecondSec,
+    inViewThirdSec,
+    inViewFourthSec,
+    inViewFifthSec,
+    fadeTimeout,
   };
-  const firstSectionHeight = mobile ? 800 : tablet ? 800 : 800;
   return (
-    <Grid style={{ flex: 1 }}>
+    <Grid style={{ flex: 1, position: "relative" }}>
       <TopNav {...pageProps} />
 
       <Grid
@@ -120,6 +184,7 @@ function App() {
         container
         xs={12}
         style={{ backgroundColor: "#E9ECF2", paddingTop: 32, ...padding }}
+        ref={secondSectionRef}
       >
         <SecondSection {...pageProps} />
       </Grid>
@@ -128,6 +193,7 @@ function App() {
         container
         xs={12}
         style={{ backgroundColor: "white", paddingTop: 32, ...padding }}
+        ref={thirdSectionRef}
       >
         <ThirdSection {...pageProps} />
       </Grid>
@@ -140,6 +206,7 @@ function App() {
           paddingTop: 32,
           ...padding,
         }}
+        ref={fourthSectionRef}
       >
         <FourthSection {...pageProps} />
       </Grid>
@@ -148,6 +215,7 @@ function App() {
         container
         xs={12}
         style={{ backgroundColor: "#E9ECF2", paddingTop: 32, ...padding }}
+        ref={fifthSectionRef}
       >
         <FifthSection {...pageProps} />
       </Grid>
@@ -232,6 +300,8 @@ const SecondSection = ({
   desktop,
   laptop,
   textAlignment,
+  inViewSecondSec,
+  fadeTimeout,
 }) => {
   return (
     <Grid
@@ -246,24 +316,30 @@ const SecondSection = ({
         //   flexDirection: "row-reverse",
       }}
     >
-      <Grid
-        item
-        container
-        xs={12}
-        md={5}
-        justifyContent={tablet ? "center" : null}
+      <Fade
+        in={inViewSecondSec}
+        timeout={fadeTimeout}
+        easing={{ enter: "cubic-bezier(0.37, 0.36, 0.67, 1.01)" }}
       >
-        <img
-          src={tablet ? smallFunds : funds}
-          alt="me"
-          style={{
-            width: photoHeight,
-            height: photoHeight,
-            top: tablet ? 10 : 80,
-            position: "relative",
-          }}
-        />
-      </Grid>
+        <Grid
+          item
+          container
+          xs={12}
+          md={5}
+          justifyContent={tablet ? "center" : null}
+        >
+          <img
+            src={tablet ? smallFunds : funds}
+            alt="me"
+            style={{
+              width: photoHeight,
+              height: photoHeight,
+              top: tablet ? 10 : 80,
+              position: "relative",
+            }}
+          />
+        </Grid>
+      </Fade>
       <Grid item xs={12} md={5}>
         <h1 style={{ ...textAlignment }}>
           <span className="boxed-span">Create </span>Funds and{" "}
@@ -285,6 +361,8 @@ const ThirdSection = ({
   tablet,
   desktop,
   laptop,
+  inViewThirdSec,
+  fadeTimeout,
 }) => {
   return (
     <Grid
@@ -312,26 +390,32 @@ const ThirdSection = ({
           amount, competitors allowed, max stocks allowed and more!
         </p>
       </Grid>
-      <Grid
-        item
-        container
-        xs={12}
-        md={7}
-        justifyContent={tablet ? "center" : null}
+      <Fade
+        in={inViewThirdSec}
+        timeout={fadeTimeout}
+        easing={{ enter: "cubic-bezier(0.37, 0.36, 0.67, 1.01)" }}
       >
-        <img
-          src={tablet ? smallCompPhoto : compPhotos}
-          alt="me"
-          style={{
-            width: photoHeight,
-            height: photoHeight,
-            top: tablet ? 10 : 80,
-            position: "relative",
-            // borderRadius: 5,
-            // filter: "brightness(80%)",
-          }}
-        />
-      </Grid>
+        <Grid
+          item
+          container
+          xs={12}
+          md={7}
+          justifyContent={tablet ? "center" : null}
+        >
+          <img
+            src={tablet ? smallCompPhoto : compPhotos}
+            alt="me"
+            style={{
+              width: photoHeight,
+              height: photoHeight,
+              top: tablet ? 10 : 80,
+              position: "relative",
+              // borderRadius: 5,
+              // filter: "brightness(80%)",
+            }}
+          />
+        </Grid>
+      </Fade>
     </Grid>
   );
 };
@@ -343,6 +427,8 @@ const FourthSection = ({
   tablet,
   desktop,
   laptop,
+  inViewFourthSec,
+  fadeTimeout,
 }) => {
   return (
     <Grid
@@ -355,26 +441,32 @@ const FourthSection = ({
         // flexDirection: "row-reverse",
       }}
     >
-      <Grid
-        item
-        container
-        xs={12}
-        md={5}
-        justifyContent={tablet ? "center" : null}
+      <Fade
+        in={inViewFourthSec}
+        timeout={fadeTimeout}
+        easing={{ enter: "cubic-bezier(0.37, 0.36, 0.67, 1.01)" }}
       >
-        <img
-          src={tablet ? smallCommunity : community}
-          alt="me"
-          style={{
-            width: photoHeight,
-            height: photoHeight,
-            top: tablet ? 0 : 80,
-            position: "relative",
-            // borderRadius: 5,
-            // filter: "brightness(80%)",
-          }}
-        />
-      </Grid>
+        <Grid
+          item
+          container
+          xs={12}
+          md={5}
+          justifyContent={tablet ? "center" : null}
+        >
+          <img
+            src={tablet ? smallCommunity : community}
+            alt="me"
+            style={{
+              width: photoHeight,
+              height: photoHeight,
+              top: tablet ? 0 : 80,
+              position: "relative",
+              // borderRadius: 5,
+              // filter: "brightness(80%)",
+            }}
+          />
+        </Grid>
+      </Fade>
       <Grid item xs={12} md={5}>
         <h1 style={{ color: "white", ...textAlignment }}>
           Browse the{" "}
@@ -398,6 +490,8 @@ const FifthSection = ({
   tablet,
   desktop,
   laptop,
+  inViewFifthSec,
+  fadeTimeout,
 }) => {
   return (
     <Grid
@@ -419,32 +513,67 @@ const FifthSection = ({
           performance.Will you come out on top?{" "}
         </p>
       </Grid>
-      <Grid
-        item
-        container
-        xs={12}
-        md={7}
-        justifyContent={tablet ? "center" : null}
-        //  style={{ backgroundColor: "red", justifyContent: "center" }}
+      <Fade
+        in={inViewFifthSec}
+        timeout={fadeTimeout}
+        easing={{ enter: "cubic-bezier(0.37, 0.36, 0.67, 1.01)" }}
       >
-        <img
-          src={tablet ? smallEmpire : empire}
-          alt="me"
-          style={{
-            width: photoHeight,
-            height: photoHeight,
-            top: tablet ? 10 : 80,
-            position: "relative",
-            // borderRadius: 5,
-            // filter: "brightness(80%)",
-          }}
-        />
-      </Grid>
+        <Grid
+          item
+          container
+          xs={12}
+          md={7}
+          justifyContent={tablet ? "center" : null}
+          //  style={{ backgroundColor: "red", justifyContent: "center" }}
+        >
+          <img
+            src={tablet ? smallEmpire : empire}
+            alt="me"
+            style={{
+              width: photoHeight,
+              height: photoHeight,
+              top: tablet ? 10 : 80,
+              position: "relative",
+              // borderRadius: 5,
+              // filter: "brightness(80%)",
+            }}
+          />
+        </Grid>
+      </Fade>
     </Grid>
   );
 };
 
 const TopNav = ({ mobile, tablet, desktop, laptop }) => {
+  const [stickyClass, setStickyClass] = useState("");
+
+  useEffect(() => {
+    window.addEventListener("scroll", stickNavbar);
+    return () => window.removeEventListener("scroll", stickNavbar);
+  }, []);
+
+  function stickNavbar() {
+    if (window !== undefined) {
+      let windowHeight = window.scrollY;
+      //use a percent of the windowheight so can work on mobile
+      windowHeight > 150 ? setStickyClass("sticky-nav") : setStickyClass("");
+    }
+  }
+
+  function goToAppStore() {
+    window.open(
+      "https://apps.apple.com/us/app/stock-market-kings/id1618162738"
+    );
+  }
+
+  function scrollToSection(fromTopDistance) {
+    setTimeout(() => {
+      window.scrollTo({
+        top: fromTopDistance || 750,
+        behavior: "smooth",
+      });
+    }, 500);
+  }
   return (
     <Grid
       item
@@ -452,10 +581,13 @@ const TopNav = ({ mobile, tablet, desktop, laptop }) => {
       style={{
         height: 80,
         alignItems: "center",
-        borderBottom: "1.5px solid ",
+        //  borderBottom: "1.5px solid ",
         paddingLeft: 8,
         paddingRight: 8,
+        //  position: "sticky",
+        zIndex: 100,
       }}
+      className={`${stickyClass}`}
     >
       <Grid
         item
@@ -471,8 +603,16 @@ const TopNav = ({ mobile, tablet, desktop, laptop }) => {
         }}
         xs={12}
         md={6}
+        onClick={() => {
+          setTimeout(() => {
+            window.scrollTo({
+              top: 0,
+              behavior: "smooth",
+            });
+          }, 500);
+        }}
       >
-        <img src={appIcon} alt="TuffHammer Logo" style={{ height: "100%" }} />
+        <img src={appIcon} alt="Stock Logo" style={{ height: "100%" }} />
         <h2 style={{ marginLeft: 8 }}>Stock Market Kings</h2>
       </Grid>
       <Hidden mdDown>
@@ -486,26 +626,33 @@ const TopNav = ({ mobile, tablet, desktop, laptop }) => {
           xs={6}
         >
           <Hidden smDown>
-            <Button onClick={() => {}}>
+            <Button onClick={() => scrollToSection(800)}>
               <p className="nav-btns">Invest</p>
             </Button>
+            <Button onClick={() => scrollToSection(1600)}>
+              <p className="nav-btns">Compete</p>
+            </Button>
+            <Button onClick={() => scrollToSection(2400)}>
+              <p className="nav-btns">Build</p>
+            </Button>
+            <Button className="nav-btns">
+              <p className="nav-btns" onClick={goToAppStore}>
+                Download
+              </p>
+            </Button>
             <Button
+              className="nav-btns"
               onClick={() => {
                 setTimeout(() => {
                   window.scrollTo({
-                    top: 750,
+                    //this is how you scroll to bottom of page, used timeout so the new pages coordinates can be used(w/o timeout will scroll as far as prior page went down)
+                    top: document.body.scrollHeight,
                     behavior: "smooth",
                   });
                 }, 500);
               }}
             >
-              <p className="nav-btns">Compete</p>
-            </Button>
-            <Button className="nav-btns">
-              <p className="nav-btns">Download</p>
-            </Button>
-            <Button className="nav-btns">
-              <p className="nav-btns">Contact</p>
+              <p className="nav-btns">Follow Us</p>
             </Button>
           </Hidden>
           <Hidden mdUp>
